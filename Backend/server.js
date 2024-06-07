65a6ccc4ecce47f14804ec7c2856dced9d20e0dc
Ajitkumar-25 <kajit0408@gmail.com>
2024-06-07 13:25:05 +0530

    project setup done

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const cors = require('cors');
+const connectDB=require('./database/connection')
+
+connectDB();
+app.use(cors({origin: true, credentials: true}));
+app.use(express.json());
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT ? process.env.PORT : 5000, () => {
+    console.log(`Server listening on PORT ${process.env.PORT}`);
+  })
